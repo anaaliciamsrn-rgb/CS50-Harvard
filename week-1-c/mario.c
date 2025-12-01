a095adc3fcbf4dfba064b2b0ff8315e437b3ddce
anaaliciamsrn-rgb <anaaliciamsrn@gmail.com>
2025-11-30 21:11:00 -0300

    Mario pyramid solution

--- week-1-c/mario.c
+++ week-1-c/mario.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
+#include <cs50.h>
+int main (void)
+{
+    int altura;
 
+    do
+{
+
+
+    altura = get_int ("Qual o seu número para altura (1-8):");
+}
+
+while (altura<1|| altura>8);
+ printf("Você escolheu: %d\n", altura);
+
+for (int linha=0; linha<altura; linha++)
+{
+    for(int bloco=0; bloco < altura-linha-1; bloco++)
+{
+    printf(" ");
+}
+for(int z=0; z<=linha; z++)
+{
+ printf("#");
+}
+
+      printf("\n");
+}
+
+
+    return 0;
+}
